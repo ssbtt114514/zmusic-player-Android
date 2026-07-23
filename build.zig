31969cba1a5ddfcab67c736a5eab022319dbfc55
ssbtt114514 <ssbtt114514@gmail.com>
2026-07-24 07:02:27 +0800

    更新 build.zig

--- build.zig
+++ build.zig
@@ -70,9 +70,9 @@ pub fn build(b: *std.Build) void {
     shared_lib.root_module.addImport("player", player_mod_for_lib);
 
     // Android: 在共享库模块级别添加库搜索路径
+    // Zig 0.16.0: addLibraryPath 已从 Build 移到 Module/CompileStep
     if (target.result.os.tag == .linux and target.result.abi == .android) {
         if (b.graph.environ_map.get("ANDROID_LIB_DIR")) |lib_dir| {
-            // Zig 0.16.0: addLibraryPath 已移到 Module/CompileStep 上，不再在 Build 上
             shared_lib.root_module.addLibraryPath(b.path(lib_dir));
         }
     }
@@ -194,9 +194,10 @@ fn createMiniaudioModule(
                 .x86_64 => "x86_64-linux-android",
                 else => null,
             };
-            translate.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{ sysroot, "usr/include" }) });
+            // Zig 0.16.0: .cwd_relative 已改为 .absolute
+            translate.addSystemIncludePath(.{ .absolute = b.pathJoin(&.{ sysroot, "usr/include" }) });
             if (arch_include) |arch| {
-                translate.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{ sysroot, "usr/include", arch }) });
+                translate.addSystemIncludePath(.{ .absolute = b.pathJoin(&.{ sysroot, "usr/include", arch }) });
             }
         }
     }
@@ -311,9 +312,10 @@ fn linkPlatformLibs(b: *std.Build, mod: *std.Build.Module, target: std.Build.Res
             };
 
             if (sdk) |path| {
-                mod.addFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ path, "System/Library/Frameworks" }) });
-                mod.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{ path, "usr/include" }) });
-                mod.addLibraryPath(.{ .cwd_relative = b.pathJoin(&.{ path, "usr/lib" }) });
+                // Zig 0.16.0: .cwd_relative 已改为 .absolute
+                mod.addFrameworkPath(.{ .absolute = b.pathJoin(&.{ path, "System/Library/Frameworks" }) });
+                mod.addSystemIncludePath(.{ .absolute = b.pathJoin(&.{ path, "usr/include" }) });
+                mod.addLibraryPath(.{ .absolute = b.pathJoin(&.{ path, "usr/lib" }) });
                 // Zig 交叉编译 macOS 时 -lc 链接的是 Zig 自带 libc，不含 iconv。
                 // 需要显式链接系统的 libiconv（SDK 的 usr/lib/libiconv.tbd）。
                 mod.linkSystemLibrary("iconv", .{});
